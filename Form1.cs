7b01b4082d7afc2d4d94d1aed4693408eca6552d
Минаков Николай <minakov_nikolai@mail.ru>
2021-04-05 15:48:07 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace IT_praktika_9
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics g = e.Graphics; // Объявляетсяобъект Graphics
+                               // Далее вставляется код рисования
+      Rectangle rect = new Rectangle(330, 200, 210, 30);
+      Pen BlackPen = new Pen(Color.Red, 60);
+      Pen blackPen = new Pen(Color.Red, 5);
+      SolidBrush BLACKPen = new SolidBrush(Color.Black);
+      SolidBrush grayBrush = new SolidBrush(Color.Gray);
+      SolidBrush blueBrush = new SolidBrush(Color.SkyBlue);
+      SolidBrush graybBrush = new SolidBrush(Color.LightGray);
+      e.Graphics.FillRectangle(blueBrush, 0, 0, 2000, 200);
+      e.Graphics.FillRectangle(graybBrush, 0, 200, 2000, 800);
+      e.Graphics.DrawRectangle(BlackPen, rect);
+      e.Graphics.DrawRectangle(blackPen, 370, 120, 150, 50 );
+      e.Graphics.FillEllipse(grayBrush, 330, 230, 60, 60);
+      e.Graphics.FillEllipse(grayBrush, 480, 230, 60, 60);
+      e.Graphics.FillRectangle(BLACKPen, 350, 240, 20, 40);
+
+      while(true)
+      {
+        _pause(10);
+        e.Graphics.FillEllipse(grayBrush, 330, 230, 60, 60);
+        e.Graphics.FillRectangle(BLACKPen, 340, 250, 40, 20);
+        _pause(10);
+        e.Graphics.FillEllipse(grayBrush, 330, 230, 60, 60);
+        e.Graphics.FillRectangle(BLACKPen, 350, 240, 20, 40);
+      }
+
+    }  
+    private void _pause(int value)
+    { 
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+      while (sw.ElapsedMilliseconds < value)
+        Application.DoEvents();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
+ 
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("IT_praktika_9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IT_praktika_9")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c995bc6e-9d0c-48e9-b50e-476f62180d45")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
